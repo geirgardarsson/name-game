17257b35958e6770125edc96c20d7f5c9d279dc6
Geir Gardarsson <geir93@gmail.com>
2025-07-22 21:25:26 +0000

    config api in .env

--- web/src/config.ts
+++ web/src/config.ts
@@ -1,6 +1,6 @@
-export const API_BASE_URL = "localhost:5056";
-const WS = "ws"; // Use "wss" when deploying the app
-const HTTP = "http"; // Use "https" when deploying the app
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "localhost:5056";
+const WS = import.meta.env.VITE_WS || "ws"; // Use "wss" when deploying the app
+const HTTP = import.meta.env.VITE_HTTP || "http"; // Use "https" when deploying the app
 
 export const API_URL = `${HTTP}://${API_BASE_URL}/api`;
 export const WS_URL = `${WS}://${API_BASE_URL}/api/ws`;
